2b3227a3a617d28a35567fe6e9d371c601e0efae
skappler <steffen.kappler@gmx.de>
2017-02-11 15:22:26 +0100

    Add new parser 2

--- src/core/game.cpp
+++ src/core/game.cpp
@@ -145,71 +145,210 @@ void Game::debugPrint(void)
 
 bool Game::load(string filename, string &loadmsg)
 {
-    ifstream infile (filename.c_str());
-    int MAX=80;
-    char buffer[MAX+1];
-    int x, y, z, found, xsize, ysize;
+    tyyval result;
+    FILE *loadfile;
+    int x, y;
 
-    //Erste Zeile wird eingelesen und Spielfeld Groesse ausglesen
+    loadfile = fopen(filename.c_str(), "r");
 
-    infile.getline(buffer, MAX);
-
-    found = string(buffer).find("x=") + 2;
-    if (isdigit(buffer[found]))
-    {
-        xsize = atoi (buffer + found);
+    if(loadfile==NULL){
+        loadmsg = "Error: Fileloading not sucesfull!";
+        return false;
     }
 
-    found = string(buffer).find("y=") + 2;
-    if (isdigit(buffer[found]))
-    {
-        ysize = atoi (buffer + found);
+    do {
+        lexikal(loadfile, &result);
+        if (result.type == L_VARIABLE && result.s == "size"){
+            lexikal(loadfile, &result);
+            if (result.type == L_INT){
+                m_board->updateDimension(result.i);            //Quadratisches Spielfeld
+                m_board->clear();
+            }
+        }
+        else if (result.type == L_ADRESS && result.s == "x"){
+            x = result.i;
+        }
+        else if (result.type == L_ADRESS && result.s == "y"){
+            y = result.i;
+        }
+        else if (result.type == L_VARIABLE && result.s == "z"){
+            lexikal(loadfile, &result);
+            if (result.type == L_INT){
+                m_board->setTile(y, x, result.i);
+            }
+        }
     }
+    while (result.type != L_FILEEND);
 
-    m_board->updateDimension(xsize);            //Quadratisches Spielfeld
-    m_board->clear();
+    fclose(loadfile);
 
 
-    infile.getline(buffer, MAX);            //Zweite Zeile ueberspringen
 
-    //Es werden solange die Spielfelddaten eingelesen bis das Ende erreicht wurde
+//    ifstream infile (filename.c_str());
+//    int MAX=80;
+//    char buffer[MAX+1];
+//    int x, y, z, found, xsize, ysize;
 
-    while (!infile.eof())
-    {
-        infile.getline(buffer, MAX);
+//    //Erste Zeile wird eingelesen und Spielfeld Groesse ausglesen
 
-        found = string(buffer).find("x") + 1;
+//    infile.getline(buffer, MAX);
 
-        if (isdigit(buffer[found]))
-        {
-            x = atoi (buffer + found);      //String in Zahl umwandeln
-        }
+//    found = string(buffer).find("x=") + 2;
+//    if (isdigit(buffer[found]))
+//    {
+//        xsize = atoi (buffer + found);
+//    }
 
-        found = string(buffer).find("y") + 1;
-        if (isdigit(buffer[found]))
-        {
-            y = atoi (buffer + found);
-        }
+//    found = string(buffer).find("y=") + 2;
+//    if (isdigit(buffer[found]))
+//    {
+//        ysize = atoi (buffer + found);
+//    }
 
-        found = string(buffer).find("z=") + 2;
-        if (isdigit(buffer[found]))
-        {
-            z = atoi (buffer + found);
-        }
+//    m_board->updateDimension(xsize);            //Quadratisches Spielfeld
+//    m_board->clear();
 
-        if ((x >= xsize) || (y >= xsize))
-        {
-            loadmsg = "Error: To much data!";
-            return false;
-        }
-        else
-        {
-            m_board->setTile(y, x, z);
-        }
 
-    }
+//    infile.getline(buffer, MAX);            //Zweite Zeile ueberspringen
+
+//    //Es werden solange die Spielfelddaten eingelesen bis das Ende erreicht wurde
+
+//    while (!infile.eof())
+//    {
+//        infile.getline(buffer, MAX);
+
+//        found = string(buffer).find("x") + 1;
+
+//        if (isdigit(buffer[found]))
+//        {
+//            x = atoi (buffer + found);      //String in Zahl umwandeln
+//        }
+
+//        found = string(buffer).find("y") + 1;
+//        if (isdigit(buffer[found]))
+//        {
+//            y = atoi (buffer + found);
+//        }
+
+//        found = string(buffer).find("z=") + 2;
+//        if (isdigit(buffer[found]))
+//        {
+//            z = atoi (buffer + found);
+//        }
+
+//        if ((x >= xsize) || (y >= xsize))
+//        {
+//            loadmsg = "Error: To much data!";
+//            return false;
+//        }
+//        else
+//        {
+//            m_board->setTile(y, x, z);
+//        }
+
+//    }
 
     loadmsg = "Loading sucessfull!";
     return true;
+
 }
+
+bool Game::lexikal(FILE *inf, tyyval *yyval)
+{
+    int c = 0;
+    lexstate s;
+    s = L_START;
+
+    while(c != EOF)
+    {
+        c = getc(inf);
+
+        switch(s){
+            case L_START:
+                yyval->s = "";
+                yyval->si = "";
+                yyval->i = 0;
+                if (isdigit(c)){
+                    s = L_INT;
+                    yyval->si.append (1, c);
+                }
+                else if (isalpha(c)){
+                    s = L_IDENT;
+                    yyval->s.append (1, c);
+                }
+            break;
+            case L_INT:
+                if (isdigit(c)){
+                    s = L_INT;
+                    yyval->si.append (1, c);
+                }
+                else{
+                    //s = L_START;
+                    ungetc(c, inf);
+                    yyval->type = s;
+                    yyval->i = atoi(yyval->si.c_str());
+                    return (true);
+                    //printf("Number: %d\n", yyval.i);
+                }
+                break;
+            case L_IDENT:     //Hier wird geprueft, ob es sich um eine Adresse oder Variable handelt
+                if (isdigit(c)){
+                    s = L_ADRESS;
+                    yyval->si.append (1, c);
+                }
+                else if (isalpha(c)){
+                    s = L_VARIABLE;
+                    yyval->s.append(1, c);
+                }
+                else if (c == 61){          //Fuer Variablen mit einem Buchstaben, 61= "="
+                    //s = L_START;
+                    yyval->type = L_VARIABLE;
+                    return (true);
+                    //printf("Variable: %s\n", yyval.s.c_str());
+                }
+                else{
+                    printf("ERROR input\n");
+                    s = L_START;
+                }
+                break;
+            case L_ADRESS:
+                if (isdigit(c)){
+                    yyval->si.append (1, c);
+                }
+                else if(c == 44){
+                    yyval->i = atoi(yyval->si.c_str());
+                    yyval->type = s;
+                    return (true);
+                    //printf("Adress: %s%d\n", yyval.s.c_str(), yyval.i);
+                    //s = L_START;
+                }
+                else{
+                    printf("ERROR input\n");
+                    s = L_START;
+                }
+                break;
+            case L_VARIABLE:
+                if (isalpha(c)){
+                    yyval->s.append(1, c);
+                }
+                else if(c == 61){
+                    yyval->type = s;
+                    return (true);
+                    //printf("Variable: %s\n", yyval.s.c_str());
+                    //s = L_START;
+                }
+                else{
+                    printf("ERROR input\n");
+                    s = L_START;
+                }
+                break;
+        }
+
+    }
+
+    yyval->type = L_FILEEND;
+    return true;
+
+}
+
 
